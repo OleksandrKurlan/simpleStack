71086934f039cbc617ac3e73ad01163e6c9d0f74
sashkya <1procariot@gmail.com>
2020-07-16 15:11:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace simlpeStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(10);
+
+            Console.WriteLine(stack.Capasity());
+            stack.IsEmpty();
+            stack.Pop();
+            for (int i = 0; i <= stack.Capasity(); i++)
+            {
+                stack.Push(i);
+            }
+            stack.IsFull();
+            for (int i = 0; i < stack.Capasity(); i++)
+            {
+                Console.WriteLine(stack.Pop()); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simlpeStack
+{
+    class Stack
+    {
+        private readonly int[] stack;
+        private int top;
+
+        public Stack(int size)
+        {
+            stack = new int[size];
+            top = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == 0;
+        }
+
+        public bool IsFull()
+        {
+            return top == stack.Length;
+        }
+
+        public void Push(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full!");
+                return;
+            }
+
+            stack[top] = item;
+            top++;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty!");
+                return 0;
+            }
+
+            top--;
+            return stack[top];
+        }
+
+        public int Capasity()
+        {
+            return stack.Length;
+        }
+    }
+}
